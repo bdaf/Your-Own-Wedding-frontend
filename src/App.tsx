63123b2c586d4c20c3e6e701905af139471927f0
bdaf <kamilkorenkiewiczpw@gmail.com>
2023-12-05 09:11:17 +0100

    Routing moved from main to App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,33 @@
-import "./App.css";
-import { Route } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import AllSEOffers from "./pages/AllSEOffers.tsx";
+import NewSEOffers from "./pages/NewSEOffers.tsx";
+import Favourites from "./pages/Favourites.tsx";
+import ErrorPage from "./ErrorPage.tsx";
 
-import AllSEOffers from "./pages/AllSEOffers";
-import NewSEOffers from "./pages/NewSEOffers";
-import Favourites from "./pages/Favourites";
+import "./App.css";
 
 function App() {
-  return (
-    <>
-      <Route path="/">
-        <AllSEOffers />
-      </Route>
-    </>
-  );
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <AllSEOffers />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: "/se-offers",
+      element: <AllSEOffers />,
+    },
+    {
+      path: "/se-offers/new",
+      element: <NewSEOffers />,
+    },
+    {
+      path: "/se-offers/favourites",
+      element: <Favourites />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,35 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import "./index.css";
-import AllSEOffers from "./pages/AllSEOffers.tsx";
-import NewSEOffers from "./pages/NewSEOffers.tsx";
-import Favourites from "./pages/Favourites.tsx";
-import ErrorPage from "./ErrorPage.tsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <AllSEOffers />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/se-offers",
-    element: <AllSEOffers />,
-  },
-  {
-    path: "/se-offers/new",
-    element: <NewSEOffers />,
-  },
-  {
-    path: "/se-offers/favourites",
-    element: <Favourites />,
-  },
-]);
+import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
