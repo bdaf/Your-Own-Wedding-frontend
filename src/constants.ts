4529d5e82f9ebf33a671fa6a008d768166ba9675
bdaf <kamilkorenkiewiczpw@gmail.com>
2023-12-15 17:47:45 +0100

    Added API constants regarding authentication

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,7 @@
 export const API_URL = process.env.NODE_ENV === "test" ? "http://mocked-api-url" : import.meta.env.VITE_API_URL;
 export const SE_OFFERS = import.meta.env.VITE_SE_OFFERS;
 export const API_OFFERS = import.meta.env.VITE_API_SE_OFFERS;
+export const API_REGISTER = import.meta.env.VITE_API_REGISTER;
+export const API_LOGIN = import.meta.env.VITE_API_LOGIN;
+export const API_LOGGED_IN = import.meta.env.VITE_API_LOGGED_IN;
+export const API_LOGOUT = import.meta.env.VITE_API_LOGOUT;
