7a29dc179a5e7c6f191e12997ab60c2a82496141
bdaf <kamilkorenkiewiczpw@gmail.com>
2024-01-18 11:55:10 +0100

    Basic edit page without any logic - the same as new offer page created

---
+++ src/pages/SEOfferEdit.tsx
@@ -0,0 +1,25 @@
+import { useContext, useEffect } from "react";
+import NewSEOfferForm from "../components/offers/SEOfferNewForm";
+import AuthenticationContext from "../store/authentication-context";
+import { OFFERS } from "../constants";
+import { useNavigate } from "react-router-dom";
+
+function NewSEOffer() {
+  const navigate = useNavigate();
+  const authCtx = useContext(AuthenticationContext);
+
+  useEffect(() => {
+    if (!authCtx.isSupportUser()) {
+      navigate(`${OFFERS}`);
+    }
+  }, []);
+
+  return (
+    <div>
+      <div className="title">Edit SEOffer</div>
+      <NewSEOfferForm />
+    </div>
+  );
+}
+
+export default NewSEOffer;
